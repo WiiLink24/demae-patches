7386753a3bbfdb7296008144d323310d9ac6f5d1
Noah Pistilli <noah@wiilink.ca>
2025-07-30 12:22:34 -0400

    feat: Remove default ID check due to WiiLink Accounts

--- default_id.cpp
+++ default_id.cpp
@@ -11,20 +11,6 @@ constexpr char error_message[] = "Please use a NAND dump from a real console.\nF
  * If it is, it displays a message telling the user to dump their NAND.
  */
 void DefaultIDCheck() {
-  es::ESInit();
-
-  u32 console_id{};
-  es::ES_GetDeviceId(&console_id);
-
-#if (!BETA && !DEV)
-  if (console_id == DEFAULT_DOLPHIN_ID) {
-    RVL::OSWritePanic(reinterpret_cast<void *>(0x804725a0),
-                      reinterpret_cast<void *>(0x80474580),
-                      error_message);
-    __builtin_unreachable();
-  }
-#endif
-
   // If we are good, copy the Wii Number into an area we can use for later.
   u64 friend_code{};
   nwc24::NWC24GetMyUserId(&friend_code);
